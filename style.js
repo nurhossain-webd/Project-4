4ab62ec89794e47be071fcbc30d1640e37c4c061
Md Nur Hossain Riyad <md_nur_hossain.riyad@smail.th-koeln.de>
2026-02-23 19:22:49 +0100

    change js file to make availabel jobs buttons functional

--- style.js
+++ style.js
@@ -1,5 +1,6 @@
 // get the element by id name
 let totalJobs = document.getElementById('totalJobs')
+let mainInterviewedJobs = document.getElementById('mainInterviewedJobs')
 let availableJobs = document.getElementById('availableJobs')
 let allBtn = document.getElementById('allBtn')
 let interviewBtn = document.getElementById('interviewBtn')
@@ -34,24 +35,195 @@ function showJobs(id) {
     const selected = document.getElementById(id)
     selected.classList.remove('hidden')
 }
-const addInterviewedJobs = [];
-const interviwedJobsDiv = document.getElementById('addNewIntJobs');
-function renderIntJobsDiv() {
-    interviwedJobsDiv.innerHTML = addInterviewedJobs.join('');
-}
 
-// function for main part btn functionality
+let addInterviewedJobs = [];
+let addRejectedJobs = [];
+
 document.querySelector('main').addEventListener('click', function (event) {
+
     if (event.target.classList.contains('intSelectBtn')) {
         const jobCard = event.target.parentNode.parentNode.parentNode.parentNode.parentNode;
+        const jobTitle = jobCard.querySelector('.jobTitle ').innerText;
+        const jobRole = jobCard.querySelector('.jobRole ').innerText;
+        const jobType = jobCard.querySelector('.jobType ').innerText;
+        const statusBtn = jobCard.querySelector('.statusBtn ').innerText;
+        const jobDescription = jobCard.querySelector('.jobDescription ').innerText;
 
-        jobCard.querySelector('.statusBtn').innerHTML = `<button
-                                        class="intSelectBtn btn btn-active text-green-500 border-2 border-green-500">INTERVIEW</button>`;
-
-        const html = jobCard.innerHTML;
-        if (!addInterviewedJobs.includes(html)) {
-            addInterviewedJobs.push(html);
-            renderIntJobsDiv();
+        jobCard.querySelector('.statusBtn ').innerText = 'INTERVIEW'
+        const cardInfo = {
+            jobTitle,
+            jobRole,
+            jobType,
+            statusBtn: 'INTERVIEW',
+            jobDescription
         }
+        const jobExist = addInterviewedJobs.find(item => item.jobTitle == cardInfo.jobTitle)
+        jobCard.querySelector('.statusBtn ').innerText = 'INTERVIEW'
+        if (!jobExist) {
+            addInterviewedJobs.push(cardInfo)
+        }
+        addRejectedJobs = addRejectedJobs.filter(item => item.jobTitle != cardInfo.jobTitle)
+        renderaddInterviewedJobs()
+        renderaddRejectedJobs()
+
+        // add total interviewed jobs available
+        mainInterviewedJobs.innerText = addInterviewedJobs.length
     }
+    if (event.target.classList.contains('rejectMarkBtn')) {
+        const jobCard = event.target.parentNode.parentNode.parentNode.parentNode.parentNode;
+        const jobTitle = jobCard.querySelector('.jobTitle ').innerText;
+        const jobRole = jobCard.querySelector('.jobRole ').innerText;
+        const jobType = jobCard.querySelector('.jobType ').innerText;
+        const statusBtn = jobCard.querySelector('.statusBtn ').innerText;
+        const jobDescription = jobCard.querySelector('.jobDescription ').innerText;
+
+        jobCard.querySelector('.statusBtn ').innerText = 'REJECTED'
+        const cardInfo = {
+            jobTitle,
+            jobRole,
+            jobType,
+            statusBtn: 'REJECTED',
+            jobDescription
+        }
+        const jobExist = addRejectedJobs.find(item => item.jobTitle == cardInfo.jobTitle)
+
+        if (!jobExist) {
+            addRejectedJobs.push(cardInfo)
+        }
+        addInterviewedJobs = addInterviewedJobs.filter(item => item.jobTitle != cardInfo.jobTitle)
+        renderaddRejectedJobs()
+        renderaddInterviewedJobs()
+        // add total interviewed jobs available
+        mainInterviewedJobs.innerText = addInterviewedJobs.length
+    }
+
 })
+
+function renderaddInterviewedJobs() {
+    interviwedJobs.innerHTML = ''
+
+    for (let interviewedJob of addInterviewedJobs) {
+        let div = document.createElement('div');
+        div.className = 'space-y-15'
+        div.innerHTML = `
+        <div  class="flex justify-between p-4 rounded hover:bg-gray-200">
+                        <div class="space-y-5 ">
+                            <h1 class="jobTitle font-bold mt-5">${interviewedJob.jobTitle}</h1>
+                            <p class="text-black/70 jobRole">${interviewedJob.jobRole}</p>
+                            <p class="text-black/70 jobType">${interviewedJob.jobType}</p>
+                            <div class="statusBtn ">
+                                <button class="btn btn-active">${interviewedJob.statusBtn}</button>
+                            </div>
+
+                            <p class="text-black/70 jobDescription">${interviewedJob.jobDescription}</p>
+                            <div class="flex gap-2">
+                                <div>
+                                    <button
+                                        class="intSelectBtn btn btn-active text-green-500 border-2 border-green-500">INTERVIEW</button>
+                                </div>
+                                <div>
+                                    <button
+                                        class="rejectMarkBtn btn btn-active text-red-500 border-2 border-red-500">REJECTED</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <a role="button" class="btn"> <i class="fa-regular fa-trash-can"></i></a>
+                        </div>
+                    </div>
+        `
+        interviwedJobs.appendChild(div)
+
+    }
+}
+function renderaddRejectedJobs() {
+    rejectedJobs.innerHTML = ''
+
+    for (let rejectedJob of addRejectedJobs) {
+        let div = document.createElement('div');
+        div.className = 'space-y-15'
+        div.innerHTML = `
+        <div  class="flex justify-between p-4 rounded hover:bg-gray-200">
+                        <div class="space-y-5 ">
+                            <h1 class="jobTitle font-bold mt-5">${rejectedJob.jobTitle}</h1>
+                            <p class="text-black/70 jobRole">${rejectedJob.jobRole}</p>
+                            <p class="text-black/70 jobType">${rejectedJob.jobType}</p>
+                            <div class="statusBtn ">
+                                <button class="btn btn-active">${rejectedJob.statusBtn}</button>
+                            </div>
+
+                            <p class="text-black/70 jobDescription">${rejectedJob.jobDescription}</p>
+                            <div class="flex gap-2">
+                                <div>
+                                    <button
+                                        class="intSelectBtn btn btn-active text-green-500 border-2 border-green-500">INTERVIEW</button>
+                                </div>
+                                <div>
+                                    <button
+                                        class="rejectMarkBtn btn btn-active text-red-500 border-2 border-red-500">REJECTED</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <a role="button" class="btn"> <i class="fa-regular fa-trash-can"></i></a>
+                        </div>
+                    </div>
+        `
+        rejectedJobs.appendChild(div)
+
+    }
+}
+
+
+// // Interviewed Job arry and render function
+// let addInterviewedJobs = [];
+// const interviwedJobsDiv = document.getElementById('addNewIntJobs');
+// function renderIntJobsDiv() {
+//     interviwedJobsDiv.innerHTML = addInterviewedJobs.join('');
+// }
+
+// // rejected job array and render function
+// let addRejectedJobs = [];
+// const rejectedJobsDiv = document.getElementById('addNewRejectedJobs');
+// function renderRejectedJobsDiv() {
+//     rejectedJobsDiv.innerHTML = addRejectedJobs.join('');
+// }
+
+// // function for main part btn functionality
+// document.querySelector('main').addEventListener('click', function (event) {
+//     // for Interviewed Btn
+//     if (event.target.classList.contains('intSelectBtn')) {
+//         let jobCard = event.target.parentNode.parentNode.parentNode.parentNode.parentNode;
+
+
+
+//         let html = jobCard.innerHTML;
+//         addRejectedJobs = addRejectedJobs.filter(item => item !== html);
+//         renderRejectedJobsDiv();
+//         jobCard.querySelector('.statusBtn').innerHTML = `<button
+//                                         class="intSelectBtn btn btn-active text-green-500 border-2 border-green-500">INTERVIEW</button>`;
+//         html = jobCard.innerHTML;
+//         if (!addInterviewedJobs.includes(html)) {
+//             addInterviewedJobs.push(html);
+//             renderIntJobsDiv();
+//         }
+
+//     }
+//     // for rejected Btn
+//     if (event.target.classList.contains('rejectMarkBtn')) {
+//         const jobCard = event.target.parentNode.parentNode.parentNode.parentNode.parentNode;
+
+//         let html = jobCard.innerHTML;
+//         addInterviewedJobs = addInterviewedJobs.filter(item => item !== html);
+//         renderIntJobsDiv();
+
+//         jobCard.querySelector('.statusBtn').innerHTML = `<button
+//                                         class="rejectMarkBtn btn btn-active text-red-500 border-2 border-red-500">REJECTED</button>`;
+
+//         html = jobCard.innerHTML;
+//         if (!addRejectedJobs.includes(html)) {
+//             addRejectedJobs.push(html);
+//             renderRejectedJobsDiv();
+//         }
+//     }
+// })
